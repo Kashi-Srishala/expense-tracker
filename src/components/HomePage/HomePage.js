bf139629c4509652c37095a02592ed2137f985ed
Kashi-Srishala <kashisrishala@gmail.com>
2023-05-29 01:26:25 +0530

    update

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,7 +4,6 @@ const HomePage = ({ fullName, profilePhotoUrl }) => {
   return (
     <div className="profile-page">
       <h2>Welcome, {fullName}!</h2>
-      <img src={profilePhotoUrl} alt="Profile" />
       <p>This is the homepage content.</p>
     </div>
   );
